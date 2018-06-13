90ead975e19ae51de0243cbd2ef8fd58c9dea0bc
vijay <vijaysankarshan@gmail.com>
2018-06-13 15:16:11 -0400

    first commit

---
+++ src/rectangle.java
@@ -0,0 +1,21 @@
+public class rectangle
+
+{
+	public int length;
+	public int width;
+
+	public rectangle(int length, int width)
+	{
+		this.length=length;
+		this.width=width;
+	}
+	public int getArea()
+	{
+		return length * width;
+	}
+	publicc int get Perimeter()
+	{
+		return 2*(length+width);
+	}
+}
+
---
+++ src/rectangulator.java
@@ -0,0 +1,15 @@
+public class rectangulator
+{
+	public static void main(string[] args)
+	{
+		int length = Integer.parseInt(args[0]);
+		int width = Integer.parseInt(args[1]);
+
+		rectangle myrectangle = new rectangle(length, width);
+
+		string output = string.format("***length: %d \n width: %d \n area: %d \n preimeter: %d")
+				system.out.println(output);
+
+
+}
+
